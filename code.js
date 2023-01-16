c51e1048f60dd6ddc4eb0fd80cba9addea5ef368
callum <116295240+callumjt@users.noreply.github.com>
2023-01-16 20:23:06 +0000

    side

---
+++ code.js
@@ -0,0 +1,9 @@
+function Open() {
+    document.getElementById("mySidebar").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+}
+    
+function Close() {
+    document.getElementById("mySidebar").style.width = "0";
+    document.getElementById("main").style.marginLeft= "0";
+}
