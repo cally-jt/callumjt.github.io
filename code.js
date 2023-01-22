01a3bd1301dcb75d3e52bec4e5c99c6b525cd6e2
callum <116295240+callumjt@users.noreply.github.com>
2023-01-22 17:08:42 +0000

    a lot

--- code.js
+++ code.js
@@ -9,3 +9,23 @@ function Close() {
     document.getElementById("open").style.opacity = "100%";
 	document.getElementById("close").style.opacity = "0%";
 }
+
+ //Selectors
+ const text = document.querySelector(".text");
+ const image = document.querySelector(".image");
+ 
+ window.addEventListener("scroll",appearOnScroll);
+ 
+ 
+ function appearOnScroll(){
+     let textPosition = text.getBoundingClientRect().top ;
+     let windowHeight = window.innerHeight/1;
+     if(windowHeight>textPosition){
+         text.classList.remove("push-left");
+         image.classList.remove("push-right");
+     }else{
+         text.classList.add("push-left");
+         image.classList.add("push-right");
+     }
+ 
+ } 
